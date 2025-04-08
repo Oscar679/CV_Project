be8b11edcaf86448df8bd7c569814fbbea9e2e5f
Oscar Ekberg <oscarekberg@hotmail.com>
2025-04-08 15:22:53 +0200

    s

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
 require('dotenv').config();
-
+console.log('tes1231231t');
 const app = express();
 const port = 3000;
 
