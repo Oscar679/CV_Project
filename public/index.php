1187748e709dd54d4f56d7a9858795223442acea
Oscar Ekberg <oscarekberg@hotmail.com>
2025-04-13 14:14:39 +0200

    commented scripts

--- public/index.php
+++ public/index.php
@@ -22,12 +22,12 @@ if (!$result) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"> <!-- problem med css inladdning -->
-    <!-- <script src="https://unpkg.com/typeit/dist/index.umd.js"></script> -->
+    <!-- <link rel="stylesheet" href="style.css">problem med css inladdning
+     <script src="https://unpkg.com/typeit/dist/index.umd.js"></script>
     <script src="src/Main.js" language="javascript" type="text/javascript"></script>
     <script src="src/Content.js" language="javascript" type="text/javascript"></script>
     <script src="src/ElemUtil.js" language="javascript" type="text/javascript"></script>
-    <script src="src/SectionChecker.js" language="javascript" type="text/javascript"></script>
+    <script src="src/SectionChecker.js" language="javascript" type="text/javascript"></script> -->
     <!-- <script src="src/Query.js" language="javascript" type="text/javascript"></script> -->
     <title>This is me - Oscar Ekberg</title>
 </head>
