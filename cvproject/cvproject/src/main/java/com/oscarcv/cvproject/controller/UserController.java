4aa81afe3f0b2dec4589aa27e4900e09740cf8f0
Oscar Ekberg <oscarekberg@hotmail.com>
2025-04-27 21:13:18 +0200

    fixed mapping

--- cvproject/cvproject/src/main/java/com/oscarcv/cvproject/controller/UserController.java
+++ cvproject/cvproject/src/main/java/com/oscarcv/cvproject/controller/UserController.java
@@ -3,11 +3,13 @@ package com.oscarcv.cvproject.controller;
 import com.oscarcv.cvproject.entity.User;
 import com.oscarcv.cvproject.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -16,7 +18,7 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/api/users")
+    @GetMapping("/users")
     public List<User> getAllUsers() {
         return userRepository.findAllUsers();
     }
